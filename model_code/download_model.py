81565fd7e6b2ccf304b0ccbece154a3adca28148
TumblerWarren <thewarrendummy@gmail.com>
2023-09-07 19:48:31 +0530

    All OK

--- model_code/download_model.py
+++ model_code/download_model.py
@@ -1,7 +1,13 @@
 from huggingface_hub import hf_hub_download
-
+import os
 
 REPO_ID = "TheBloke/CodeLlama-13B-Python-GGML"
 FILENAME = "codellama-13b-python.ggmlv3.Q2_K.bin"
+LOCAL_DIR = "../Resource/Model"  # Use forward slashes for directory paths
 
-hf_hub_download(repo_id=REPO_ID, filename=FILENAME,local_dir = r"..\Resource\Model")
+# Check if the local directory exists, and if not, create it
+if not os.path.exists(LOCAL_DIR):
+    os.makedirs(LOCAL_DIR)
+
+# Download the model to the local directory
+hf_hub_download(repo_id=REPO_ID, filename=FILENAME, local_dir=LOCAL_DIR)
