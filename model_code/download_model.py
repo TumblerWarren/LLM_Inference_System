cc967914e067acf61f1947fb519e0351b395af70
TumblerWarren <thewarrendummy@gmail.com>
2023-09-14 14:31:24 +0530

    All OK

--- model_code/download_model.py
+++ model_code/download_model.py
@@ -1,6 +1,7 @@
 from huggingface_hub import hf_hub_download
 import os
-print("Initiating Model Download!")
+
+
 REPO_ID = "TheBloke/Pygmalion-13B-SuperHOT-8K-GGML"
 FILENAME = "pygmalion-13b-superhot-8k.ggmlv3.q4_K_S.bin"
 LOCAL_DIR = "../Resource/Model"  # Use forward slashes for directory paths
@@ -9,6 +10,14 @@ LOCAL_DIR = "../Resource/Model"  # Use forward slashes for directory paths
 if not os.path.exists(LOCAL_DIR):
     os.makedirs(LOCAL_DIR)
 
-# Download the model to the local directory
-hf_hub_download(repo_id=REPO_ID, filename=FILENAME, local_dir=LOCAL_DIR)
-print("MODEL DOWNLOAD COMPLETE")
+# Check if the model file already exists in the local directory
+model_path = os.path.join(LOCAL_DIR, FILENAME)
+if not os.path.exists(model_path):
+    print("Initiating Model Download!")
+    # Download the model to the local directory
+    hf_hub_download(repo_id=REPO_ID, filename=FILENAME, local_dir=LOCAL_DIR)
+    print("MODEL DOWNLOAD COMPLETE")
+else:
+    print("Model already exists in the local directory. Skipping download.")
+
+# Now you can proceed with using the model from `model_path` if it exists.
